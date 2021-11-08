adb4f6876fe181d7b476d4cc453491686d9d598e
shashisad <saadshashi25@gmail.com>
2021-11-08 14:52:07 +0530

    done minor changes

---
+++ frontend/src/Pages/Feedback.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import style from '../assets/css/Form.module.css';
+const Feedback = () => {
+    return (
+        <>
+        
+       </>
+    )
+}
+
+export default Feedback
